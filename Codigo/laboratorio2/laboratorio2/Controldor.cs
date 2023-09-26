3a87c753c28a5f3d7bfb56def070cd997850bbb0
DiegoMaroquin <98416906+DiegoMaroquin@users.noreply.github.com>
2023-09-26 10:13:05 -0500

    boton1 9491-20-7815

---
+++ Codigo/laboratorio2/laboratorio2/Controldor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using laboratorio2;
+using System.Data.Odbc;
+using System.Data;
+
+
+namespace laboratorio2
+{
+    public class Controldor
+    {
+       sentensias sn = new sentensias();
+
+            {
+            string consulta = "select * from  alumnos";
+            OdbcDataAdapter dt = sn.llenartb2(consulta);
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+        public void CargarTablas(ComboBox comboBox1, string BD)
+        {
+            OdbcDataAdapter dt = sn.buscartbl(BD);
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            int contador = 0;
+            comboBox1.Items.Clear();
+            foreach (DataRow Row in table.Rows)
+            {
+                comboBox1.Items.Add(table.Rows[contador][0].ToString());
+                contador++;
+            }
+
+
+        }
+    }
+    
---
+++ Codigo/laboratorio2/modelo/conexion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace modelo
+{
+  
+        public class Conexion
+        {
+            //Función: Clase de conexión por medio de ODBC
+            public OdbcConnection conexion()
+            {
+
+
+                OdbcConnection conn = new OdbcConnection("Dsn=umg_prototipo2k23");
+                try
+                {
+                    conn.Open();
+                }
+                catch (OdbcException)
+                {
+                    Console.WriteLine("No Conectó");
+                }
+                return conn;
+            }
+
+
+            public void desconexion(OdbcConnection conn)
+            {
+                try
+                {
+                    conn.Close();
+                }
+                catch (OdbcException)
+                {
+                    Console.WriteLine("No Conectó");
+                }
+            }
+        }
+
+
+
+    }
+
